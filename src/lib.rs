931c2af324f391f3bc9640fd5a6a6d173e7db079
Igor null <m1el.2027@gmail.com>
2020-08-12 23:44:12 +0300

    added license file copied from honggfuzz

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,26 @@
-//! Basic fuzzer mutation strategies
+//! Basic fuzzer mutation strategies, largely ported from honggfuzz
+/*
+ *
+ * Authors:
+ * Robert Swiecki <swiecki@google.com>
+ * Brandon Falk <bfalk@gamozolabs.com>
+ *
+ * Copyright 2010-2018 by Google Inc. All Rights Reserved.
+ * Copyright 2020 by Brandon Falk <bfalk@gamozolabs.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ */
 
 // No need for std
 #![no_std]
